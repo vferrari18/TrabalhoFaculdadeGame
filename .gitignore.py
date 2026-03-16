57c786b85f51634f81c38fd219e0cf8c774a10e1
Ferrari <ferrari@outlook.com.br>
2026-03-16 20:15:20 -0300

    Finalização

---
+++ .gitignore.py
@@ -0,0 +1,22 @@
+```gitignore
+# Python - Arquivos temporários
+__pycache__/
+*.py[cod]
+*$py.class
+
+# Ambiente Virtual
+.venv/
+venv/
+ENV/
+
+# Arquivo de Ranking Local
+score.json
+
+# Distribuição / Executável
+dist/
+build/
+*.spec
+
+# IDEs
+.vscode/
+.idea/
